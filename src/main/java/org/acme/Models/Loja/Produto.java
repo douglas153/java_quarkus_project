f4106fc082403387b0f81c0706ba2e537b407c85
douglas153 <57470495+douglas153@users.noreply.github.com>
2024-01-10 14:09:32 -0300

    Create Produto.java

---
+++ src/main/java/org/acme/Models/Loja/Produto.java
@@ -0,0 +1,45 @@
+package org.acme.Models.Loja;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "produtos") //Usado quando o nome da classe é diferente da tabela no banco.
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // dizer como o valor é gerado;
+    private Long id;
+    private String nome;
+    private String descricao;
+    private BigDecimal preco;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+}
