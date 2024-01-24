d8e01d66b1e41ff9c2357372b97e84d896c0353e
douglas153 <57470495+douglas153@users.noreply.github.com>
2024-01-24 14:05:44 -0300

    Create Funcionario.java

---
+++ src/main/java/org/acme/Models/Loja/Funcionario.java
@@ -0,0 +1,57 @@
+package org.acme.persistence.dao;
+
+import java.math.BigDecimal;
+
+public class Funcionario {
+    private String nome;
+    private String cpf;
+    private double salario;
+    private int tipo = 0;
+    //COMPORTAMERNTOS: buscar bonificação;
+    public double getBonificacao(){
+        //validar tipo de funcionario para bonificação;
+        if(tipo == 0){ //Funcionario comum
+            return this.salario * 0.1;
+        } else if (tipo == 1) { //Gerente
+            return this.salario * 0.2;
+        } else if (tipo == 2) { //Diretor
+            return this.salario * 0.3;
+        }else {
+            return this.salario;
+        }
+    }
+
+    //GETTERS AND SETTERS
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+}
+
+
