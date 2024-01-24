c51f4bcc7b82e9983f57203ff04dbf4b890d66d9
douglas153 <57470495+douglas153@users.noreply.github.com>
2024-01-24 14:06:11 -0300

    Create Gerente.java

---
+++ src/main/java/org/acme/Models/Loja/Gerente.java
@@ -0,0 +1,29 @@
+package org.acme.persistence.dao;
+
+public class Gerente  extends Funcionario{
+    private int senha;
+
+    public Gerente(int senha) {
+        this.senha = senha;
+    }
+
+    //VALIDAR AUTENTICAÇÃO
+    public boolean autenticar(int senha){
+        if(this.senha == senha){
+            System.out.println("aut" + senha + this.senha);
+            return true;
+        }else{
+            System.out.println("aut" + senha + this.senha);
+            return false;
+        }
+    }
+    //GETTERS AND SETTERS
+    public int getSenha() {
+        return senha;
+    }
+
+    public void setSenha(int nome) {
+        this.senha = nome;
+    }
+
+}
